a7d47d21b11491c585b2aef87819931e482974f1
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-09-15 15:39:11 +0100

    punishment

--- api/app.py
+++ api/app.py
@@ -1,37 +1,43 @@
 from flask import Flask, jsonify, request
+import time
+from datetime import datetime
+
 
 app = Flask(__name__)
 
 
+
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    return '<h2>WELCOME TO 0X3DEVOID API</h2>'
-
-
-
-@app.route('/api', methods=['GET', 'POST'])
 def apiroute():
+    current_time_utc = datetime.utcnow()
+    current_day = current_time_utc.strftime('%A')
+    current_time = current_time_utc.strftime('%H:%M:%S')
       
     if request.method == 'GET':
-        name = request.args.get('slack_name')
-        day = request.args.get('current_day')
-        if name and day:
-              data = {
-            'slack_name': name,
-            'current_day': day,
-            'track': 'backend',
-            'github_repo': 'https://github.com/Halfboyfriend?tab=repositories',
-            'github_file_url': 'https://github.com/Halfboyfriend/Hng-stage-one.git',
-            'status_code': 200
-            
-
-        }
-           
-        else:
-             data = {'error': 'Please parse in parameters (slack_name and current_day)', 'status_code': 404}
-
+          
+          data = {
+               'full_name': 'Owoyemi Idris  Olamilekan',
+                'age': 22,
+                'slack_name': '0x3Devoid',
+                'current_day': current_day,
+                "utc_time": current_time,
+                'track': 'backend',
+                'github_repo_url': 'https://github.com/Halfboyfriend',
+                'contact': '08170585143',
+                'status': 'serving punishment',
+                'status_code': 200 }
     return jsonify(data)
 
+@app.route('/calculate', methods=['POST'])
+def calculate():
+     if not request.json:
+          return jsonify({'error': 'invalid input'}), 400
+     input_data = request.json.get('data')
+     if not isinstance(input_data, int):
+          return jsonify({'error': 'Input is not an integer'}), 400
+
+     result = input_data * 2
+     return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,17 @@
+# import time
+# from datetime import datetime
+
+# # Get the current time in UTC
+# current_time_utc = datetime.utcnow()
+
+# # Get the current day of the week
+# current_day = current_time_utc.strftime('%A')
+
+# # Get the current time separately
+# current_time = current_time_utc.strftime('%H:%M:%S')
+
+# print(f"Current Day: {current_day}")
+# print(f"Current Time (UTC): {current_time}")
+# print(current_time_utc)
+
+import requests
